c62393941c705158f3e83a96762907a7d76cd8f2
akbhartomo <akbhartomo@gmail.com>
2025-06-19 14:19:10 +0700

    [home] init page home

---
+++ src/app/home/page.tsx
@@ -0,0 +1,8 @@
+export default async function Page() {
+  // This is the home page, you can add any content you want here.
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">Welcome to the Next Shadcn Dashboard Starter!</h1>
+    </div>
+  );
+}
