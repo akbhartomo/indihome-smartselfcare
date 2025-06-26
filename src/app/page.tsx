735b41bec8d294b944b4bca9540b6cc9954c28cb
akbhartomo <akbhartomo@gmail.com>
2025-06-26 14:48:48 +0700

    init home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
-  const { userId } = await auth();
+  // const { userId } = await auth();
 
-  if (!userId) {
-    return redirect('/auth/sign-in');
-  } else {
-    redirect('/dashboard/overview');
-  }
+  // if (!userId) {
+  //   return redirect('/auth/sign-in');
+  // } else {
+  //   redirect('/dashboard/overview');
+  // }
+
+  return redirect('/home');
 }
